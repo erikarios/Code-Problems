9b006a3cb0a8163a62774466e1c2fa2c6314d850
Erika Rios <erikarios@erikas-air.home>
2022-05-14 17:58:08 -0700

    solution003 completed

---
+++ js/solution003.js
@@ -0,0 +1,4 @@
+/*Your job is simple, if (x) squared is more than 1000, return 'It's hotter than the sun!!', else, return 'Help yourself to a honeycomb Yorkie for the glovebox.' X will be a valid integer number.
+X will be either a number or a string. Both are valid.*/
+
+const apple = x => (x = Math.pow(x,2)) > 1000 ? 'It\'s hotter than the sun!!' : 'Help yourself to a honeycomb Yorkie for the glovebox.'
