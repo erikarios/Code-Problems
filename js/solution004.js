f56e2da14cd947d3c5d3637bf1f8db2e61374644
Erika Rios <erikarios@Erikas-MacBook-Air.local>
2022-05-16 10:51:14 -0700

    solution004

---
+++ js/solution004.js
@@ -0,0 +1,12 @@
+/*Return Negative
+You are given a number and have to make it negative. But maybe the number is already negative?.*/
+
+//Math.abs() - returns absolute vaule of a number
+//Two different solutions to the same problem
+
+function makeNegative(num) {
+    return (-Math.abs(num))
+}
+
+
+makeNegative = num => (-Math.abs(num))
