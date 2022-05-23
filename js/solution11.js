9f3b01a3fe241225d39484393429649f4fac9a83
Erika Rios <rios.erika.tatiana@gmail.com>
2022-05-22 21:49:05 -0700

    solution11

---
+++ js/solution11.js
@@ -0,0 +1,15 @@
+/*String repeat
+Write a function called repeatStr which repeats the given string string exactly n times
+*/
+
+//n represents the number
+//s represents the string
+//s.repeat() will repeat the string n times
+
+const repeatStr = (n, s) => s.repeat(n)
+
+
+/*Test Case
+repeatStr(6, "I") // "IIIIII"
+repeatStr(5, "Hello") // "HelloHelloHelloHelloHello"
+*/
