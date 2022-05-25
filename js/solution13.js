a9d978b7055935a6c1ff8ca17557c9df3b79ab44
Erika Rios <rios.erika.tatiana@gmail.com>
2022-05-24 23:38:47 -0700

    solution13

---
+++ js/solution13.js
@@ -0,0 +1,13 @@
+/*5 without numbers !!
+
+Write a function that always returns 5. Just bear in mind that you can't use any of the following characters: 0123456789*+-/
+*/
+
+//Since numbers cannot be used a five letter string was used instead
+//.length is used to return the number of characters present in the string, which will always be 5
+
+function unusualFive() {
+    return 'bambi'.length
+  }
+
+
