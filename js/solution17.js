7e70aa0685c0ed5325e1a6200f919662ce6f10f5
Erika Rios <rios.erika.tatiana@gmail.com>
2022-05-27 21:36:14 -1000

    solution17

---
+++ js/solution17.js
@@ -0,0 +1,17 @@
+/*Convert a String to a Number!
+
+We need a function that can transform a string into a number. 
+Note: All inputs will be strings, and every string is a perfectly valid representation of an integral number.
+
+*/
+
+// Number() converts a string to the number type. If the value can't be converted, it returns NaN
+//parseInt() can also be used to convert the string to the number
+
+
+const stringToNumber = str => Number(str)
+
+
+/*Test Cases
+stringToNumber('45') => Output: 45
+*/
