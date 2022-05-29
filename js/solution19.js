82ab13e208ed48a17cd3ba1593888a5dc8a7762c
Erika Rios <rios.erika.tatiana@gmail.com>
2022-05-29 00:10:50 -0700

    solution19

---
+++ js/solution19.js
@@ -0,0 +1,14 @@
+/*Convert a Number to a String!
+We need a function that can transform a number into a string.
+*/
+
+//.toString() method in Javascript is used with a number and converts the number to a string
+
+function numberToString(num) {
+    return num.toString();
+  }
+
+
+/*Test Cases
+numberToString(9) => Output: '9'
+*/
