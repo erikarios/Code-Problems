a3da66b929ffc2cdc1c114de9e1872780b239655
Erika Rios <rios.erika.tatiana@gmail.com>
2022-06-16 21:07:00 -0700

    solution 39

---
+++ js/solution39.js
@@ -0,0 +1,13 @@
+/*Volume of a Cuboid
+
+Write a function that calculates the volume of a cuboid*/
+
+//To find the volume you need to multiply length * width * volume
+
+volumeOfCuboid = (l,w,h) => l * w * h
+  
+/*Test Cases   
+volumeOfCuboid(2, 5, 6) => Output: 60
+volumeOfCuboid(6.3, 3, 5) => Output: 94.5
+*/
+
