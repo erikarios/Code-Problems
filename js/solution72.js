1896231ff2daa39c992b514bd2c88b3d576ac522
Erika Rios <rios.erika.tatiana@gmail.com>
2022-07-15 21:05:47 -0700

    solution 72

---
+++ js/solution72.js
@@ -0,0 +1,12 @@
+/* Function 2 - squaring an argument
+
+Now you have to write a function that takes an argument and returns the square of it */
+
+// The parameter is an integer
+// We need to return the integer squared
+
+const square = n => n **2
+
+/*Test Cases   
+square(3) => Output: 9
+*/
