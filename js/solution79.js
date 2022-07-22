e32cd22a51767ab5bab157aac25524a21749214c
Erika Rios <rios.erika.tatiana@gmail.com>
2022-07-21 18:23:21 -0700

    solution 79

---
+++ js/solution79.js
@@ -0,0 +1,38 @@
+/* Welcome
+
+Your start-up's BA has told marketing that your website has a large audience in Scandinavia and surrounding countries. Marketing thinks it would be great to welcome visitors to the site in their own language. Luckily you already use an API that detects the user's location, so this is an easy win
+
+Write a 'welcome' function that takes a parameter 'language' (always a string), and returns a greeting - if you have it in your database. It should default to English if the language is not in the database, or in the event of an invalid input */
+
+// The parameter is a string that reflects a language 
+// We need to return 'welcome' in the language that was provided. If the language is not in our database or the parameter is invalid, we return 'welcome' in English.  
+// Make an object that holds the languages 
+
+function greet(lang) {
+    return langs[lang]||langs['english'];
+}
+  
+  const langs = {
+  english: 'Welcome',
+  czech: 'Vitejte',
+  danish: 'Velkomst',
+  dutch: 'Welkom',
+  estonian: 'Tere tulemast',
+  finnish: 'Tervetuloa',
+  flemish: 'Welgekomen',
+  french: 'Bienvenue',
+  german: 'Willkommen',
+  irish: 'Failte',
+  italian: 'Benvenuto',
+  latvian: 'Gaidits',
+  lithuanian: 'Laukiamas',
+  polish: 'Witamy',
+  spanish: 'Bienvenido',
+  swedish: 'Valkommen',
+  welsh: 'Croeso'
+};
+
+/*Test Cases   
+greet('english') => Output: 'Welcome'
+greet('dutch') => Output: 'Welkom'
+*/
