edd41aefea1d0b46d1577f935de42c34990661b1
Erika Rios <rios.erika.tatiana@gmail.com>
2022-07-30 21:43:48 -0700

    solution 87

---
+++ js/solution87.js
@@ -0,0 +1,22 @@
+/* Messi's Goal Total
+
+Use variables to find the sum of the goals Messi scored in 3 competitions
+
+Information
+Messi goal scoring statistics:
+La Liga	43
+Champions League	10
+Copa del Rey	5
+
+Create these three variables and store the appropriate values using the table above:
+laLigaGoals, championsLeagueGoals, and copaDelReyGoals. Create a fourth variable named totalGoals that stores the sum of all of Messi's goals for this year. */
+
+// The parameters are the three goals in the different leagues which are given to us
+// We want to return the total
+
+let laLigaGoals = 43
+let championsLeagueGoals = 10
+let copaDelReyGoals = 5
+
+let totalGoals = laLigaGoals + championsLeagueGoals + copaDelReyGoals
+
