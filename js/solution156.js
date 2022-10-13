53ac15d22d758531bd229c6f1ed0b9137ae8f137
Erika Rios <rios.erika.tatiana@gmail.com>
2022-10-12 22:42:52 -0700

    solution 156

--- js/solution156.js
+++ js/solution156.js
@@ -2,7 +2,7 @@
 
 Write a function called checkCoupon which verifies that a coupon code is valid and not expired. A coupon is no more valid on the day AFTER the expiration date. All dates will be passed as strings in this format: "MONTH DATE, YEAR" */
 
-
+// We are given four string parameters, two of the strings are numbers and two of the strings are dates
 
 function checkCoupon(enteredCode, correctCode, currentDate, expirationDate){
     return enteredCode === correctCode && Date.parse(expirationDate) >= Date.parse(currentDate)
