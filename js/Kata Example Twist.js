a476a3883595535ca542350f0989e75d813177a3
Erika Rios <rios.erika.tatiana@gmail.com>
2022-12-10 11:47:00 -0800

    Codewars - Kata Example Twist

---
+++ js/Kata Example Twist.js
@@ -0,0 +1,10 @@
+/* Codewars - Kata Example Twist
+
+Add the value "codewars" to the array websites 1,000 times
+*/
+
+var websites = []
+for (let i = 0; i < 1000; i++){
+    websites.push('codewars')
+};
+
