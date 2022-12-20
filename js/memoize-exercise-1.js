0edaade3a7ff2c4dbdf694a1f6729b1897b554a7
Erika Rios <rios.erika.tatiana@gmail.com>
2022-12-20 15:27:00 -0800

    memoize-exercise-updated

--- js/memoize-exercise-1.js
+++ js/memoize-exercise-1.js
@@ -1,16 +1,14 @@
 /* memoize-exercise-1
 
-// Task 1: Write a function, times10, that takes an argument, n, and multiples n times 10
-// a simple multiplication fn
+ Task 1: Write a function, times10, that takes an argument, n, and multiples n times 10 a simple multiplication fn
 */
 
 const times10 = (n) => (n*10);
 
 //console.log('times10 returns:', times10(9));
 
-/* 
 // Task 2: Use an object to cache the results of your times10 function. 
-*/
+
 
 const cache = {};
 
@@ -33,3 +31,29 @@ console.log('Task 2 cached value:', memoTimes10(9));	// cached
 
 // Create a function that checks if the value for n has been calculated before.
 // If the value for n has not been calculated, calculate and then save the result in the cache object.
+
+// Task 3: Clean up your global scope by moving your cache inside your function.
+
+// Use a closure to return a function that you can call later.
+const memoizedClosure = () => {
+    let cache = {};
+      return (n) => {
+          if (n in cache) {
+              //console.log('Fetching from cache:', n);
+              return cache[n];
+          }
+          else {
+              //console.log('Calculating result');
+              let result = times10(n);
+              cache[n] = result;
+              return result;
+          }
+      };
+  };
+  
+  // returned function from memoizedAdd
+  const memoClosureTimes10 = memoizedClosure();
+  console.log('Task 3 calculated value:', memoClosureTimes10(9));	// calculated
+  console.log('Task 3 cached value:', memoClosureTimes10(10));	// cached
+  
+  
