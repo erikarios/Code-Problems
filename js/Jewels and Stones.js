e4df964964976f61e7f4fd93b8d4e72262a9adb5
Erika Rios <rios.erika.tatiana@gmail.com>
2022-12-30 14:42:00 -0800

    Jewels and Stones -updated pseudocode

--- js/Jewels and Stones.js
+++ js/Jewels and Stones.js
@@ -10,23 +10,25 @@ All the characters of jewels are unique.
 // The parameters are two strings
 // We need to return an integer representing the number of stones that are also jewels. So how many characters in jewels is found in stones
 // We have to compare each character of jewels to each character of stones
-// If the character of jewels is found in stones, we increase the output count by 1
 // Create a map object
-// Create an output variable and set that to 0
-// Loop through the jewels string. Put each character of jewels inside of map (set the value equal to true)
+// Create an output variale and set that to 0
+// If the character of jewels is found in stones, we increase the output count by 1
+// Loop through the jewels string. Put each character of jewels inside of map and set the value equal to true
 // Loop through the stones string. If the character of stones matches the characters of jewel, increment output variable by one
-// Return output variable
+// Return the count variable
+// Time complexity: O(jewels + stones)
+// Space complexity: O(jewels)
 
 var numJewelsInStones = function (jewels,stones){
-  const map = {}
+  const js = {}
   let count = 0
 
   for(const jewel of jewels){
-    map[jewel] = true
+    js[jewel] = true // {a: true, A: true}
   }
 
   for(const stone of stones){
-    if( map[stone] ){
+    if( stone in js ){
       count++
     }
   }
