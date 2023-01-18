54440f198b00c0f549d519332cad3291cce82b2c
Erika Rios <rios.erika.tatiana@gmail.com>
2023-01-18 13:47:56 -0800

    Max Char updated

--- js/Max Char.js
+++ js/Max Char.js
@@ -15,7 +15,8 @@ No String or Array Methods!
 // We need to return the character that appears most in the string
 // We need to loop through the string and count the characters 
 // Using an object literal, the characters are the keys and the count are the values 
-// The first loop iterates through the given string. If the same character appears in the string then one is added to the count variable
+// null value represents the intentional absence of any object value
+// The first loop iterates through the given string. If the character does not already appear in the charMap object then it is added (ex: l:1). If the character is already in the charMap object then one is added to the count (ex: l:3. This continues until the loop iterates through the entire string then one is added to the count variable
 // Then, we can iterate through the object (character map) to find the character that has the largest count
 // Return the character 
 // Time complexity is linear. Space complexity is constant
